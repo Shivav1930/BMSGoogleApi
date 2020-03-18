227b08dfe2193967218bd4455a9ff1d5c8814ff1
Hello@1a <Shivav1930>
2020-03-18 20:55:46 +0530

    initial commit

---
+++ src/main/java/utility/SheetsQuickstart.java
@@ -0,0 +1,78 @@
+package utility;
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.SheetsScopes;
+import com.google.api.services.sheets.v4.model.ValueRange;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SheetsQuickstart {
+    private static final String APPLICATION_NAME = "Google Sheets API Java Quickstart";
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+
+    private static final List<String> SCOPES = Collections.singletonList(SheetsScopes.SPREADSHEETS_READONLY);
+    private static final String CREDENTIALS_FILE_PATH = "client_secret.json";
+
+    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
+        // Load client secrets.
+        InputStream in = SheetsQuickstart.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        if (in == null) {
+            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+        }
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+    }
+
+    public static String readGoogleAPIData(String key) throws IOException, GeneralSecurityException {
+        // Build a new authorized API client service.
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        final String spreadsheetId = "1EfinnVlw8bvNYdWNhjUVYaSBq6V2iMrzB7kw-2-rpTI";
+        final String range = "Sheet1!A1:B4";
+        Sheets service = new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+        ValueRange response = service.spreadsheets().values()
+                .get(spreadsheetId, range)
+                .execute();
+        List<List<Object>> values = response.getValues();
+        Map<String, String> hm = new LinkedHashMap<String, String>();
+        if (values == null || values.isEmpty()) {
+           // System.out.println("No data found.");
+        } else {
+            //System.out.println("Name, Major");
+            for(int i=0;i<values.size() ; i++) {
+            	for(int j=0; j< values.get(i).size()-1; j++) {
+            		hm.put(values.get(i).get(j).toString(), values.get(i).get(j+1).toString());
+            	}
+            }
+            
+           
+        }
+        return hm.get(key);
+    }
+}
---
+++ src/test/java/com/bookmyshow/pom/pages/MovieTicketBooking.java
@@ -0,0 +1,210 @@
+package com.bookmyshow.pom.pages;
+
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.bookmyshow.pom.base.BasePage;
+import com.bookmyshow.pom.testcases.base.BaseTest;
+import com.bookmyshow.pom.testcases.base.ThreadLocalDriver;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
+import io.appium.java_client.MobileElement;
+
+public class MovieTicketBooking extends BaseTest  {
+	
+
+	
+	public static void SelectEngLang(ExtentTest test) throws Exception{
+		//test.log(LogStatus.INFO, "click on Letsgo");
+		BasePage.click("Letsgo",test,(ThreadLocalDriver.getTLDriver()));
+		
+		/*test.log(LogStatus.INFO, "click on Letsgo");
+		BasePage.click("Letsgo",test);*/
+		
+		test.log(LogStatus.INFO, "click on Skip");
+		BasePage.click("Skip",test,(ThreadLocalDriver.getTLDriver()));
+		
+		/*test.log(LogStatus.INFO, "click on Allow");
+		BasePage.click("Allow",test, driver);*/
+		Thread.sleep(5000);
+		
+		try {
+			if(driver.findElement (By.xpath("//android.widget.Button[@text='DENY']")) != null){	
+			 	
+				BasePage.click("Deny",test,(ThreadLocalDriver.getTLDriver()));
+			}
+		
+		} catch (Exception e) {
+			test.log(LogStatus.INFO, "Click on submit");
+			BasePage.click("ALLOW",test, (ThreadLocalDriver.getTLDriver()));
+			//e.printStackTrace();
+			Thread.sleep(5000);
+			
+		}
+		
+	}
+	
+	public static void Movies(ExtentTest test) throws Exception {
+		/*WebElement ele ;33
+		ele= (ThreadLocalDriver.getTLDriver()).findElement(By.id("com.bt.bms:id/et_search"));
+		ele.click();
+		ele.sendKeys("Chittoor");*/
+		BasePage.click("Banglore",test, (ThreadLocalDriver.getTLDriver()));
+		Thread.sleep(5000);
+		WebElement searchBar ;
+		WebDriverWait wait = new WebDriverWait((ThreadLocalDriver.getTLDriver()), 10);
+		  searchBar = (ThreadLocalDriver.getTLDriver()).findElement((By.id("com.bt.bms:id/et_search")));
+	    //wait.until(ExpectedConditions.visibilityOf(searchBar));
+	    
+	    searchBar.sendKeys("Chittoor");
+		
+		
+		/*WebElement searchBar ;
+		 searchBar = (ThreadLocalDriver.getTLDriver()).findElement(By.id("com.bt.bms:id/et_search"));
+		 searchBar.click();
+		 searchBar.sendKeys("Chittoor");*/
+		
+	/*	try {
+		    Thread.sleep(5000);
+		    WebDriverWait wait = new WebDriverWait(driver, 10);
+		    MobileElement searchBar = (MobileElement) driver.findElement(By.id("com.bt.bms:id/et_search"));
+		    wait.until(ExpectedConditions.visibilityOf(searchBar));
+		    searchBar.sendKeys("Chittoor");
+		    List<WebElement> listOfCities = driver.findElements(By.id("com.bt.bms:id/location_other_region_name"));
+		    for (WebElement cityName : listOfCities) {
+		        if (cityName.getText().equalsIgnoreCase("Chittoor")) {
+		            cityName.click();
+		            break;
+		        }
+		    }
+		} catch (InterruptedException e) {
+		    e.printStackTrace();
+		}*/
+		
+		
+		Thread.sleep(5000);
+		test.log(LogStatus.INFO, "click on Movies");
+		BasePage.click("Movies",test, (ThreadLocalDriver.getTLDriver()));
+		
+	    test.log(LogStatus.INFO, "Click on search url");
+	    BasePage.click("searchurl",test, (ThreadLocalDriver.getTLDriver()));
+	   
+	}
+	
+	public static void EnterMovieName(ExtentTest test) throws Exception {
+
+	    test.log(LogStatus.INFO, "Enter the URL");
+	    BasePage.enterText("searchurl1", "Enterurl1",test,(ThreadLocalDriver.getTLDriver()));
+	    
+	    Thread.sleep(2500);
+	    test.log(LogStatus.INFO, "Click on clickOnMovie");
+	    BasePage.click("clickOnMovie",test,(ThreadLocalDriver.getTLDriver()));
+		}
+	
+	public static void EnterSecondMovieName(ExtentTest test) throws Exception {
+		
+	    test.log(LogStatus.INFO, "Enter the URL");
+	    BasePage.enterText("searchurl1", "Enterurl",test,(ThreadLocalDriver.getTLDriver()));
+	    
+	    Thread.sleep(2500);
+	    test.log(LogStatus.INFO, "Click on clickOnMovie");
+	    BasePage.click("clickOnMovie",test,(ThreadLocalDriver.getTLDriver()));
+		}
+	
+	public static void Book(ExtentTest test) throws Exception {
+		Thread.sleep(2500); 
+		 test.log(LogStatus.INFO, "click on book the ticket");
+		 BasePage.click("Book",test,(ThreadLocalDriver.getTLDriver()));
+		 Thread.sleep(2500);
+		}
+
+	public static void DateTime(ExtentTest test) throws Exception {
+		  
+		  test.log(LogStatus.INFO, "click on the date");
+		  BasePage.click("Date",test,(ThreadLocalDriver.getTLDriver()));
+		  
+		  Thread.sleep(1500);
+		  test.log(LogStatus.INFO, "Select the Time as 10:00 PM");
+		  BasePage.click("Time",test,(ThreadLocalDriver.getTLDriver()));
+		}
+
+	public static void Tc(ExtentTest test) throws Exception {
+		  
+		  test.log(LogStatus.INFO, "Click on Accept");
+		  BasePage.click("Accept",test,(ThreadLocalDriver.getTLDriver()));
+		}
+
+	public static void Selecttheseats(ExtentTest test) throws Exception {
+	
+		  test.log(LogStatus.INFO, "Select the number of seats as 2 and click to select the seats");
+		  BasePage.click("SelectSeats",test,(ThreadLocalDriver.getTLDriver()));
+		  
+		  test.log(LogStatus.INFO, "Select the seats");
+		  BasePage.click("seats",test,(ThreadLocalDriver.getTLDriver()));
+		  
+		  test.log(LogStatus.INFO, "click on pay");
+		  BasePage.click("pay2",test,(ThreadLocalDriver.getTLDriver()));
+		}
+	
+	public static void Selecttheseats1(ExtentTest test) throws Exception {
+		
+		  test.log(LogStatus.INFO, "Select the number of seats as 2 and click to select the seats");
+		  BasePage.click("SelectSeats",test,(ThreadLocalDriver.getTLDriver()));
+		  
+		  test.log(LogStatus.INFO, "Select the seats");
+		  BasePage.click("seats1",test,(ThreadLocalDriver.getTLDriver()));
+		  
+		  test.log(LogStatus.INFO, "click on pay");
+		  BasePage.click("pay2",test,(ThreadLocalDriver.getTLDriver()));
+		}
+
+	public static void Details(ExtentTest test) throws Exception {	  
+		 		  
+	  
+		 test.log(LogStatus.INFO, "Enter your email id ");
+		 BasePage.enterText("Epath", "Email",test,(ThreadLocalDriver.getTLDriver()));
+		 
+		 test.log(LogStatus.INFO, "Enter the mobile number");
+		 BasePage.enterText("Npath", "number",test,(ThreadLocalDriver.getTLDriver()));
+		 
+		 /*test.log(LogStatus.INFO, "click on pay");
+		  BasePage.click("DoneM",test,driver);	*/
+		 
+		 Robot robot = new Robot();
+		 robot.keyPress(KeyEvent.VK_ENTER);		 
+		 robot.keyRelease(KeyEvent.VK_ENTER);
+		 
+		 test.log(LogStatus.INFO, "click on pay");
+		  BasePage.click("Done",test,(ThreadLocalDriver.getTLDriver()));
+		 
+	}
+	
+	public static void Details1(ExtentTest test) throws Exception {	  
+		  
+		  
+		 test.log(LogStatus.INFO, "Enter your email id ");
+		 BasePage.enterText("Epath", "Email",test,(ThreadLocalDriver.getTLDriver()));
+		 
+		 test.log(LogStatus.INFO, "Enter the mobile number");
+		 BasePage.enterText("Npath", "number",test,(ThreadLocalDriver.getTLDriver()));		 
+		 
+		/* test.log(LogStatus.INFO, "click on pay");
+		  BasePage.click("DoneM1",test,driver);	*/
+		 
+		 Robot robot = new Robot();
+		 robot.keyPress(KeyEvent.VK_ENTER);
+		 robot.keyRelease(KeyEvent.VK_ENTER);
+		 
+		 test.log(LogStatus.INFO, "click on pay");
+		  BasePage.click("Done",test,(ThreadLocalDriver.getTLDriver()));
+		 
+	}
+	
+
+	
+}
